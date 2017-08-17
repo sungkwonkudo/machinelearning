131bd3edb1f868f495614c103cbf7dbe0848c35d
wykiloki <sunries.k@gmail.com>
2017-08-17 10:18:33 -0500

    Added csv file, index.js modified.

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ const SLR = ml/SLR; // Simple Linear Regression
 
 const csvFilePath = 'advertising.csv'; // Data
 let csvData = [], // Parsed Data
-	X = [], // Input
-	y=[]; // Output
-	
+    X = [], // Input
+    y=[]; // Output
+    
 let regressionModel;
+
+csv()
+    .fromFile(csvFilePath)
+    .on('json',(jsonObj)=> {
+        csvData.push(jsonObj);
+    })
+    .on('done', ()=>{
+        dressData();// To get data points from JSON Objects
+        performRegression();
+    }
